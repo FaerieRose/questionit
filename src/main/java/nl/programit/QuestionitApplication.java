230b5572d85dd88816fc5ada9ca4bda107a29227
FaerieRose <rosalynn5772-git@yahoo.com>
2016-11-03 12:37:01 +0100

    Bump up the version 0.0.1-SNAPSHOT

---
+++ src/main/java/nl/programit/QuestionitApplication.java
@@ -0,0 +1,12 @@
+package nl.programit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionitApplication.class, args);
+	}
+}
