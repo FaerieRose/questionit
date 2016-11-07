9c95bc5c3999d7b5a7e325b398ecda6d0d3c45e5
FaerieRose <rosalynn5772-git@yahoo.com>
2016-11-07 13:54:39 +0100

    Exam class expanded with all necessary fields

--- src/main/java/nl/programit/domain/Exam.java
+++ src/main/java/nl/programit/domain/Exam.java
@@ -1,14 +1,105 @@
 package nl.programit.domain;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 
+/**
+ * Class for storing exam results during the exam and when the exam is completed
+ * 
+ * @author Created by Felix van Loenen, edited by FaerieRose
+ * @version v0.1
+ * @since 2016-11-03
+*/
 @Entity
 public class Exam {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
+	
+	private Date endDateTime = new Date();
+	private Date startDateTime = new Date();
+	private int timeToCompleteInSeconds;
+	
+	@OneToOne
+	private QuestionList questionList;
+	
+	@ElementCollection(fetch=FetchType.EAGER)
+	private List<Boolean> markedQuestions = new ArrayList<Boolean>();
+	
+	@OneToMany
+	private List<AnswerList> givenAnswers = new ArrayList<>();
+
+	// ---------------------------------------------------
+	// GETTER & SETTER for id
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	// ---------------------------------------------------
+	// GETTER & SETTER for endDateTime
+	public Date getEndDateTime() {
+		return endDateTime;
+	}
+	public void setEndDateTime(Date endDateTime) {
+		this.endDateTime = endDateTime;
+	}
+
+	// ---------------------------------------------------
+	// GETTER & SETTER for startDateTime
+	public Date getStartDateTime() {
+		return startDateTime;
+	}
+	public void setStartDateTime(Date startDateTime) {
+		this.startDateTime = startDateTime;
+	}
+
+	// ---------------------------------------------------
+	// GETTER & SETTER for timeToCompleteInSeconds
+	public int getTimeToCompleteInSeconds() {
+		return timeToCompleteInSeconds;
+	}
+	public void setTimeToCompleteInSeconds(int timeToCompleteInSeconds) {
+		this.timeToCompleteInSeconds = timeToCompleteInSeconds;
+	}
+
+	// ---------------------------------------------------
+	// GETTER & SETTER for questionList
+	public QuestionList getQuestionList() {
+		return questionList;
+	}
+	public void setQuestionList(QuestionList questionList) {
+		this.questionList = questionList;
+	}
+
+	// ---------------------------------------------------
+	// GETTER & SETTER for markedQuestions
+	public List<Boolean> getMarkedQuestions() {
+		return markedQuestions;
+	}
+	public void setMarkedQuestions(List<Boolean> markedQuestions) {
+		this.markedQuestions = markedQuestions;
+	}
+
+	// ---------------------------------------------------
+	// GETTER & SETTER for givenAnswers
+	public List<AnswerList> getGivenAnswers() {
+		return givenAnswers;
+	}
+	public void setGivenAnswers(List<AnswerList> givenAnswers) {
+		this.givenAnswers = givenAnswers;
+	}
 	
 }
